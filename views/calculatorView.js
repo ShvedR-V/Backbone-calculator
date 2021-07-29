cb259117e9ff26c8e4e65b5063b4de71f42a6468
ShvedR-V <vol.shvedov@gmail.com>
2021-07-29 13:47:46 +0300

    feat: add keyboard input

--- views/calculatorView.js
+++ views/calculatorView.js
@@ -3,7 +3,8 @@ const CalculatorView = Backbone.View.extend({
   template: _.template($('#calculator-template').html()),
 
   events: {
-    'click .btn': 'onClickButton'
+    'click .btn': 'onClickButton',
+    'keydown .screen': 'onKeyPress'
   },
 
   initialize: function() {
@@ -19,12 +20,30 @@ const CalculatorView = Backbone.View.extend({
     this.screen = new ScreenView({
       model: this.model
     })
-    this.$('.screen').append(this.screen.el);
+    this.$('.screen-container').append(this.screen.el);
   },
 
   onClickButton: function(event){
     const symbol = $(event.currentTarget)[0].innerText;
     const operation = $(event.currentTarget)[0].attributes.operation.value;
     this.model.getKey(operation, symbol);
+  },
+
+  validateKeyBoardPress: function(keyValue) {
+    return /^[0-9]+$/.test(keyValue)
+  },
+
+  onKeyPress: function(event) {
+    const screenValue = this.$('.screen').val();
+    if(this.validateKeyBoardPress(event.key)){
+      this.model.getKey(event.key, event.key);
+    } else {
+      event.preventDefault();
+    }
+    $(".screen").val('');
+    if (screenValue !== '0') {
+      $(".screen").val(screenValue);
+    }
+    $(".screen").focus();
   }
 });
